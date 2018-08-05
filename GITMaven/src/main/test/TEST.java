d1e1935deae2fdc8a4f2d8dd64cd31aa6c03ab9c
Duankan <729633588@qq.com>
2018-08-05 11:30:59 +0800

    redis的的依赖和简单使用

--- GITMaven/src/main/test/TEST.java
+++ GITMaven/src/main/test/TEST.java
@@ -3,7 +3,9 @@ import com.github.Duankan.dao.UsersPoMapper;
 import com.github.Duankan.dao.rolePoMapper;
 import com.github.Duankan.po.UsersPo;
 import com.github.Duankan.po.rolePo;
+import org.junit.Before;
 import org.junit.Test;
+import redis.clients.jedis.Jedis;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -14,6 +16,7 @@ public class TEST extends BaseTest{
     UsersPoMapper usersPoMapper;
     @Resource
     rolePoMapper rolePoMapper;
+    private Jedis jedis;
     @Test
     public void test(){
         System.out.println("my first git");
@@ -26,5 +29,41 @@ public class TEST extends BaseTest{
 //        System.out.println(usersPo.getUsername()+"=="+usersPo.getPassword());
         List<rolePo> rolePo =rolePoMapper.getRolesByUserid(1);
         System.out.println(rolePo.get(0).getRolename());
+    }
+
+    //测试redis链接
+    @Before
+    public void test3(){
+        //连接本地的 Redis 服务
+        jedis = new Jedis("127.0.0.1",6379);
+        System.out.println("连接成功");
+        //查看服务是否运行
+        System.out.println("服务正在运行: "+jedis.ping());
+//         增加一个Key检测是否连接成功
+//         jedis.set("foo", "bar");
+//         String value = jedis.get("foo");
+//         System.out.println("foo-->"+value);
+    }
+    //jedis存储字符串
+    @Test
+    public void jedisString(){
+        // -----添加数据----------
+        jedis.set("name", "qingqing");// 向key-->name中放入了value-->xinxin
+        System.out.println(jedis.get("name"));// 执行结果：xinxin
+
+        jedis.append("name", " is my lover"); // 拼接 。
+        // 如果用redis命令输入key对应的值是不能有空格的哦
+        System.out.println(jedis.get("name"));
+
+        jedis.del("name"); // 删除某个键
+        System.out.println(jedis.get("name"));
+        // 设置多个键值对
+        jedis.mset("name", "qingqing", "age", "23", "qq", "476777XXX");
+        jedis.incr("age"); // 进行加1操作
+        System.out.println(jedis.get("name") + "-" + jedis.get("age") + "-"
+                + jedis.get("qq"));
+
+        jedis.set("china", "这是个中文值");
+        System.out.println(jedis.get("china"));
     }
 }
--- GITMaven/src/main/webapp/WEB-INF/view/success.jsp
+++ GITMaven/src/main/webapp/WEB-INF/view/success.jsp
@@ -6,11 +6,19 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="shiro" uri="http://shiro.apache.org/tags" %>
 <html>
 <head>
     <title>认证成功！</title>
 </head>
 <body>
-认证成功！
+认证成功！<shiro:user>
+    欢迎[<shiro:principal/>]登录，<a href="${pageContext.request.contextPath}/logout">退出</a>
+</shiro:user>
+<br>
+<shiro:hasRole name="user">
+    用户[<shiro:principal/>]拥有角色user<br/>
+</shiro:hasRole>
+
 </body>
 </html>
